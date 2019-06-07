e6f6efbc9583759f64e9df11f82ef5c524d53a3f
daogunkoya <da.ogunkoya@gmail.com>
2019-06-07 15:31:24 +0100

    added face-detect

---
+++ face-detect/precache-manifest.4258887a4e589a3d6cc838cf3c2166de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d70531c0d50678656cc6459b9507b8",
+    "url": "/face-detect/index.html"
+  },
+  {
+    "revision": "86c9886003f2f3723815",
+    "url": "/face-detect/static/css/2.624d324f.chunk.css"
+  },
+  {
+    "revision": "9f7e95ad4de6f56a2d7c",
+    "url": "/face-detect/static/css/main.d8e74909.chunk.css"
+  },
+  {
+    "revision": "86c9886003f2f3723815",
+    "url": "/face-detect/static/js/2.f2f1d3e3.chunk.js"
+  },
+  {
+    "revision": "9f7e95ad4de6f56a2d7c",
+    "url": "/face-detect/static/js/main.a24eab1c.chunk.js"
+  },
+  {
+    "revision": "e2efa1fcd983fd74d445",
+    "url": "/face-detect/static/js/runtime~main.1251f083.js"
+  }
+]);
