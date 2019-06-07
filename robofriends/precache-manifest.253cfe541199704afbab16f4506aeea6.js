57f9656f292cbbe495cd04706070ac1ef740835c
daogunkoya <da.ogunkoya@gmail.com>
2019-06-07 14:54:55 +0100

    added robofriends

---
+++ robofriends/precache-manifest.253cfe541199704afbab16f4506aeea6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d746c03cf1485fe2f5ac72bf87ad82b0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "23ea21c580649473bfb7",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "86e886dd7a1dbe0c3fe1",
+    "url": "/robofriends/static/css/main.15bac0d1.chunk.css"
+  },
+  {
+    "revision": "23ea21c580649473bfb7",
+    "url": "/robofriends/static/js/2.5ce1ec96.chunk.js"
+  },
+  {
+    "revision": "86e886dd7a1dbe0c3fe1",
+    "url": "/robofriends/static/js/main.ab3dff0a.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
